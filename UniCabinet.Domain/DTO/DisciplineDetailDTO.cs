8422fdb5067b3a341451776f2336f2fb3128d490
Keks <Keks@DESKTOP-NQVSQJ6>
2024-10-04 04:01:54 +0400

    Добавление DTO моделей

---
+++ UniCabinet.Domain/DTO/DisciplineDetailDTO.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UniCabinet.Domain.Entities;
+
+namespace UniCabinet.Domain.DTO
+{
+    public class DisciplineDetailDTO
+    {
+        public int Id { get; set; }
+
+        public int DisciplineId { get; set; }
+        public Discipline Discipline { get; set; }
+
+        public int GroupId { get; set; }
+        public Group Group { get; set; }
+
+        public int SemesterId { get; set; }
+        public Semester Semester { get; set; }
+
+        public string TeacherId { get; set; }
+        public User Teacher { get; set; }
+
+        /// <summary>
+        /// Количество лекций
+        /// </summary>
+        public int LectureCount { get; set; }
+
+        /// <summary>
+        /// Количество практических
+        /// </summary>
+        public int PracticalCount { get; set; }
+
+        /// <summary>
+        /// Количество зачетов
+        /// </summary>
+        public int SubExamCount { get; set; }
+
+        /// <summary>
+        /// Количество экзаменов
+        /// </summary>
+        public int ExamCount { get; set; }
+
+        /// <summary>
+        /// Минимум посещений лекций
+        /// </summary>
+        public int MinLecturesRequired { get; set; }
+
+        /// <summary>
+        /// Минимум посещений практических
+        /// </summary>
+        public int MinPracticalsRequired { get; set; }
+
+        /// <summary>
+        /// Минимум для автомата
+        /// </summary>
+        public int AutoExamThreshold { get; set; }
+
+        /// <summary>
+        /// Минимальный балл для прохождения
+        /// </summary>
+        public int PassCount { get; set; }
+    }
+}
---
+++ UniCabinet.Domain/DTO/LectureVisitDTO.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UniCabinet.Domain.Entities;
+
+namespace UniCabinet.Domain.DTO
+{
+    public class LectureVisitDTO
+    {
+        public int Id { get; set; }
+
+        public string StudentId { get; set; }
+
+        public int LectureId { get; set; }
+
+        /// <summary>
+        /// Посещаемость
+        /// </summary>
+        public bool isVisit { get; set; }
+
+        /// <summary>
+        /// Начисленные баллы
+        /// </summary>
+        public decimal PointsCount { get; set; }
+    }
+}
