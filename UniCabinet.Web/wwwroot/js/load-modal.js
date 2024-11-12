fa5306ad17fe1fcabbc8e3bd06807db391a4f3f9
Deadlord <ali.gasanow9@gmail.com>
2024-11-12 21:39:45 +0400

    Упрощение обработки семестра и изменение способа открытия модальных окон для групп

--- UniCabinet.Web/wwwroot/js/load-modal.js
+++ UniCabinet.Web/wwwroot/js/load-modal.js
@@ -1,19 +1,27 @@
 ﻿function openModal(modalUrl, modalId, id = null) {
-     let urlWithId = modalUrl;
+    let urlWithId = modalUrl;
 
-    // Проверяем, передан ли id
+    // Если передан id, добавляем его к URL
     if (id !== null) {
-        urlWithId = `${modalUrl}?id=${id}`; // Добавляем id к URL, если он есть
+        urlWithId = `${modalUrl}?id=${id}`;
     }
 
     fetch(urlWithId)
         .then(response => response.text())
         .then(html => {
+            // Вставляем полученный HTML в контейнер
             document.getElementById("modalContainer").innerHTML = html;
-            var modal = new bootstrap.Modal(document.getElementById(modalId));
+
+            // Инициализируем и показываем модальное окно
+            const modal = new bootstrap.Modal(document.getElementById(modalId));
             modal.show();
+
+            // Удаляем модальное окно из DOM после закрытия
+            document.getElementById(modalId).addEventListener('hidden.bs.modal', () => {
+                document.getElementById("modalContainer").innerHTML = '';
+            });
         })
         .catch(error => {
-            console.error('Ошибка при открытии модального окна:', error);
+            console.error('Ошибка при загрузке модального окна:', error);
         });
 }
