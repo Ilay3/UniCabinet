14957048c646247ddd42fb01d904daa1e47f2256
Deadlord <ali.gasanow9@gmail.com>
2024-12-01 17:51:27 +0400

    Добавлено: добавление и редактирование специальностей, разграничевание моделей, создание specializationcontroller

--- UniCabinet.Core/Models/ViewModel/Specialization/SpecializationListVM.cs
+++ UniCabinet.Core/Models/ViewModel/Specialization/SpecializationListVM.cs
@@ -3,9 +3,9 @@ using UniCabinet.Core.Models.ViewModel.User;
 
 namespace UniCabinet.Core.Models.ViewModel.Specialization
 {
-    public class SpecializationVM : SpecializationBaseVM
+    public class SpecializationListVM:SpecializationBaseVM
     {
         public int Id { get; set; }
-        public List<UserVM>Teacher { get; set; }
+        public List<UserVM> Teacher { get; set; }
     }
 }
